31a1484f5b95c5a6e29e15c02955b6327a30cb27
lydiastevens <lydiastevens14@gmail.com>
2017-02-28 15:18:29 -0400

    fixed species, remerged

--- maritime_newfoundland_lydia_240217.R
+++ maritime_newfoundland_lydia_240217.R
@@ -3,7 +3,7 @@ library(rfishbase)
 library(lubridate)
 library(tidyr)
 
-newdata <- read.csv("C:/Users/StevensLy/Documents/Database/Data/newdata240217_Lydia.csv",stringsAsFactors = F,na.strings="")
+newdata <- read.csv("C:/Users/StevensLy/Documents/Database/Data/newdata240217_Lydia.csv",stringsAsFactors = F)
 head(newdata)
 
 #remove unessary columns
@@ -52,6 +52,7 @@ newdata$MONTH <- NULL
 newdata$MONTH_M <- NULL
 newdata$YEAR <- NULL
 newdata$YEAR_M <- NULL
+newdata$TIME <- NULL
 head(newdata)
 View(newdata)
 
@@ -59,13 +60,13 @@ View(newdata)
 newdata$SEASON <- toupper(newdata$SEASON)
 newdata$STRAT_TYPE <- toupper(newdata$STRAT_TYPE)
 
-#fix species before merge
-newdata[which(newdata$species=="ACIPENSER OXYRHYNCHUS"),"species"] <- "ACIPENSER OXYRINCHUS"
+#fix species column before merge
+newdata[which(newdata$species=="Acipenser oxyrhynchus"),"species"] <- "Acipenser oxyrinchus"
 newdata[which(newdata$species=="Aspidophoroides olriki"),"species"] <- "Aspidophoroides olrikii"
 newdata[which(newdata$species=="Ceratias holboelli"),"species"] <- "Ceratias holboellii"
 newdata[which(newdata$species=="Cryptopsaras couesi"),"species"] <- "Cryptopsaras couesii"
 newdata[which(newdata$species=="Pandalus propinquus"),"species"] <- "Pandalus propinqvus"
-newdata[which(newdata$species=="Scomberesox saurus saurus"),"species"] <- "Scomberesox saurus"
+newdata[which(newdata$species=="Scomberesox saurus"),"species"] <- "Scomberesox saurus saurus"
 newdata[which(newdata$species=="Stichaeus punctatus"),"species"] <- "Stichaeus punctatus punctatus"
 newdata[which(newdata$species=="Stomias boa"),"species"] <- "Stomias boa boa"
 newdata[which(newdata$fishbase=="STOMIAS BOA FEROX"),"species"] <- "Stomias boa ferox"
@@ -73,29 +74,38 @@ newdata[which(newdata$species=="Synaphobranchus kaupi"),"species"] <- "Synaphobr
 newdata[which(newdata$species=="Gymnelis viridis"),"species"] <- "Gymnelus viridis"
 newdata[which(newdata$species=="Osmerus mordax"),"species"] <- "Osmerus mordax mordax"
 newdata[which(newdata$species=="Cottunculus thompsoni"),"species"] <- "Cottunculus thomsonii"
+newdata[which(newdata$species=="Simenchelys parasiticus"),"species"] <- "Simenchelys parasitica"
+newdata[which(newdata$species=="Lycodes esmarki"),"species"] <- "Lycodes esmarkii"
+newdata[which(newdata$species=="Lumpenus lumpretaeformis"),"species"] <- "Lumpenus lampretaeformis"
+newdata[which(newdata$species=="Paraliparis copei"),"species"] <- "Paraliparis copei copei"
 
 View(newdata)
 
 #merge existing trait data with newdata
-traitdata <- read.csv("C:/Users/StevensLy/Desktop/MPA Framework/traitinformation.csv",stringsAsFactors = F,na.strings="")
-newdata2 <- merge(newdata, traitdata, all=T)
+traitdata <- read.csv("C:/Users/StevensLy/Desktop/MPA Framework/traitinformation.csv",stringsAsFactors = F)
+newdata2 <- merge(newdata, traitdata, by="species", all=T)
 View(newdata2)
 
 #sort species based on how often they were captured
-head(table(newdata$species))
+table(newdata$species)
 table(newdata$species)[which(table(newdata$species)>9000)]
-table(newdata$species)[which(table(newdata$species)<5)]
+table(newdata$species)[which(table(newdata$species)<1288)] #Less than 5% of the most captured species
+table(newdata$species)[which(table(newdata$species)>1288)] 
+
 
 
 ########trying to figure out rfishbase########
+
 options(FISHBASE_API = "http://fishbase.ropensci.org")
 fish <- newdata2$species
 fish
-species(fish, fields = fields=c("SpecCode", "PriceCateg", "Vulnerability"))
-
+species(fish[fish=="Agonus decagonus"], fields=c("SpecCode", "PriceCateg", "Vulnerability"))
+species(fish, fields=c("SpecCode", "PriceCateg", "Vulnerability"))
 species(fish[fish == "Gadus morhua"], fields = c("SpecCode"), server=getOption("http://fishbase.ropensci.org"))
 
-
+#could try installing directly from github
+#library(devtools)
+#devtools::install_github("ropensci/rfishbase@sckott-endpoint-fix")
 
 
 
