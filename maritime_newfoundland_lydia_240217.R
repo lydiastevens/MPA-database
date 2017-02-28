355059e02197914eb4e9955da7d8da09439b94c2
lydiastevens <lydiastevens14@gmail.com>
2017-02-28 11:56:34 -0400

    went back and fixed species before merging trait data

--- maritime_newfoundland_lydia_240217.R
+++ maritime_newfoundland_lydia_240217.R
@@ -3,7 +3,7 @@ library(rfishbase)
 library(lubridate)
 library(tidyr)
 
-newdata <- read.csv("C:/Users/StevensLy/Documents/Database/Data/newdata240217_Lydia.csv",stringsAsFactors = F)
+newdata <- read.csv("C:/Users/StevensLy/Documents/Database/Data/newdata240217_Lydia.csv",stringsAsFactors = F,na.strings="")
 head(newdata)
 
 #remove unessary columns
@@ -53,20 +53,48 @@ newdata$MONTH_M <- NULL
 newdata$YEAR <- NULL
 newdata$YEAR_M <- NULL
 head(newdata)
+View(newdata)
 
-#change lowercase to uppercase in colunmn "SEASONS"
+#change lowercase to uppercase in colunmn "SEASONS" and 'STRAT_TYPE"
 newdata$SEASON <- toupper(newdata$SEASON)
+newdata$STRAT_TYPE <- toupper(newdata$STRAT_TYPE)
+
+#fix species before merge
+newdata[which(newdata$species=="ACIPENSER OXYRHYNCHUS"),"species"] <- "ACIPENSER OXYRINCHUS"
+newdata[which(newdata$species=="Aspidophoroides olriki"),"species"] <- "Aspidophoroides olrikii"
+newdata[which(newdata$species=="Ceratias holboelli"),"species"] <- "Ceratias holboellii"
+newdata[which(newdata$species=="Cryptopsaras couesi"),"species"] <- "Cryptopsaras couesii"
+newdata[which(newdata$species=="Pandalus propinquus"),"species"] <- "Pandalus propinqvus"
+newdata[which(newdata$species=="Scomberesox saurus saurus"),"species"] <- "Scomberesox saurus"
+newdata[which(newdata$species=="Stichaeus punctatus"),"species"] <- "Stichaeus punctatus punctatus"
+newdata[which(newdata$species=="Stomias boa"),"species"] <- "Stomias boa boa"
+newdata[which(newdata$fishbase=="STOMIAS BOA FEROX"),"species"] <- "Stomias boa ferox"
+newdata[which(newdata$species=="Synaphobranchus kaupi"),"species"] <- "Synaphobranchus kaupii"
+newdata[which(newdata$species=="Gymnelis viridis"),"species"] <- "Gymnelus viridis"
+newdata[which(newdata$species=="Osmerus mordax"),"species"] <- "Osmerus mordax mordax"
+newdata[which(newdata$species=="Cottunculus thompsoni"),"species"] <- "Cottunculus thomsonii"
+
 View(newdata)
 
 #merge existing trait data with newdata
-traitdata <- read.csv("C:/Users/StevensLy/Desktop/MPA Framework/traitinformation.csv",stringsAsFactors = F)
-newdata2 <- merge(newdata, traitdata)
+traitdata <- read.csv("C:/Users/StevensLy/Desktop/MPA Framework/traitinformation.csv",stringsAsFactors = F,na.strings="")
+newdata2 <- merge(newdata, traitdata, all=T)
 View(newdata2)
 
 #sort species based on how often they were captured
 head(table(newdata$species))
 table(newdata$species)[which(table(newdata$species)>9000)]
 table(newdata$species)[which(table(newdata$species)<5)]
+
+
+########trying to figure out rfishbase########
+options(FISHBASE_API = "http://fishbase.ropensci.org")
+fish <- newdata2$species
+fish
+species(fish, fields = fields=c("SpecCode", "PriceCateg", "Vulnerability"))
+
+species(fish[fish == "Gadus morhua"], fields = c("SpecCode"), server=getOption("http://fishbase.ropensci.org"))
+
 
 
 
